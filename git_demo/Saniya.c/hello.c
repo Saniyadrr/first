e2722f568acba147a8cf8d34899c3c6394572e9b
Saniya <Saniya.net>
2022-11-07 20:48:25 +0530

    add folder

---
+++ git_demo/Saniya.c/hello.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main() {
+   int num1,num2;
+   printf("enter a number : ");
+   scanf("%d",&num1);
+   printf("first number is : %d  \n",num1);
+
+   printf("enter another number : ");
+   scanf("%d",&num2);
+   printf("Second number is :  %d \n",num2);
+
+   printf("addition is %d",num1 +num2 );
+
+
+}
